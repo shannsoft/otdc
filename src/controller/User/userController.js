f38f436b5ab5194f8cf33f566a1c3639a19f4e9b
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-11-24 03:32:07 +0530

    - minor change

--- src/controller/User/userController.js
+++ src/controller/User/userController.js
@@ -1,31 +1,39 @@
-app.controller('UserController',function($scope,$rootScope,$state,UserService,$localStorage,Constants){
-  // $scope.UserService = UserService;
-  $scope.init = function(){
-    $scope.user = UserService.getUser();
-    $scope.userTabs = [
-      {heading:"View Profile",active:true},
-      {heading:"Edit Profile",active:false},
-      {heading:"Change Password",active:false},
-    ]
-    $scope.currTab = 0;
-      }
-    $scope.tabChange = function(tabPos){
-      $scope.currTab = tabPos;
+app.controller('UserController', function($scope, $rootScope, $state, UserService, UtilityService,Util,$localStorage, Constants) {
+    // $scope.UserService = UserService;
+    $scope.init = function() {
+        $scope.user = UserService.getUser();
+        $scope.userTabs = [{
+            heading: "View Profile",
+            active: true
+        }, {
+            heading: "Edit Profile",
+            active: false
+        }, {
+            heading: "Change Password",
+            active: false
+        }, ]
+        $scope.currTab = 0;
     }
-    $scope.changePassword = function(){
-      var obj = {
-        UserId:$scope.user.userId,
-        TokenId:$localStorage[Constants.getTokenKey()],
-        OldPassword:$scope.user.oldPassword,
-        NewPassword:$scope.user.newPassword,
-      }
-      UserService.serviceCall().getUser(obj,function(response) {
-          console.log("Password change success");
-        },
-        function(error) {
-            console.error("Password change failed");
+    $scope.tabChange = function(tabPos) {
+        $scope.currTab = tabPos;
+    }
+    $scope.changePassword = function() {
+        var obj = {
+            UserId: $scope.user.userId,
+            TokenId: $localStorage[Constants.getTokenKey()],
+            OldPassword: $scope.user.oldPassword,
+            NewPassword: $scope.user.newPassword,
         }
-      )
+        UtilityService.showLoader();
+        UserService.serviceCall().getUser(obj, function(response) {
+                UtilityService.hideLoader();
+                Util.alertMessage("success", "Password Changed");
+            },
+            function(error) {
+                UtilityService.hideLoader();
+                Util.alertMessage("danger", "Password Change Error");
+            }
+        )
     }
 
 
