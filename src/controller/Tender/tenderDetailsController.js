256234bfa14aa89740e646cb89e22b502ae17620
rajendradbpb <rajendrasahoodbpb@gmail.com>
2016-12-15 02:51:14 +0530

    - added the boq file details

--- src/controller/Tender/tenderDetailsController.js
+++ src/controller/Tender/tenderDetailsController.js
@@ -4,25 +4,60 @@
  if the action is edit user can edit the items
  */
 
-app.controller('TenderDetailsController', function($scope, $rootScope, $state,$stateParams, ApiCall, EnvService, $timeout, $cookieStore, $localStorage) {
+app.controller('TenderDetailsController', function($scope, $rootScope, $state,$uibModal, $stateParams, ApiCall, EnvService, $timeout, $cookieStore, $localStorage) {
 
     $scope.init = function() {
-      $scope.isInit = false;
-      $stateParams.action = $stateParams.action || "view" ; // setting default action as view
-      $scope.isView = $stateParams.action == "view" ? true : false;
-      var data = {
-        tenderId : $stateParams.tenderId
-      }
-      ApiCall.getTendor(data,function(res) {
+        $scope.isInit = false;
+        $stateParams.action = $stateParams.action || "view"; // setting default action as view
+        $scope.isView = $stateParams.action == "view" ? true : false;
+        var data = {
+            tenderId: $stateParams.tenderId
+        }
+        ApiCall.getTendor(data, function(res) {
 
-        $scope.tender = res.Data;
-        // Util.alertMessage(res.Status.toLocaleLowerCase(),res.Message);
-        $scope.isInit = true;
-      }, function(err) {
-        Util.alertMessage(err.Status.toLocaleLowerCase(),err.Message);
-      })
+            $scope.tender = res.Data;
+            // Util.alertMessage(res.Status.toLocaleLowerCase(),res.Message);
+            $scope.isInit = true;
+        }, function(err) {
+            Util.alertMessage(err.Status.toLocaleLowerCase(), err.Message);
+        })
     }
-    $scope.onEditTendor = function(action,tender) {
-      $state.go("tenderDetails",{tenderId:tender.tenderId,action:action});
+    $scope.onEditTendor = function(action, tender) {
+        $state.go("tenderDetails", {
+            tenderId: tender.tenderId,
+            action: action
+        });
+    }
+    $scope.showBoq = function() {
+        $uibModal.open({
+            animation: true,
+            size: 'lg',
+            controller: "boqController",
+            templateUrl:"src/views/Tender/modals/boqDetailsModal.html",
+            resolve:{
+              boqData :function() {
+                return $scope.tender.boqData
+              }
+            }
+        });
     }
 })
+app.controller('boqController', function ($scope,$uibModalInstance,boqData) {
+  $scope.boqData = boqData;
+  $scope.getBoqHeaders = function() {
+    var temp = $scope.boqData[0];
+    var arr = []
+    // getting headers as keys present in the boq details array
+    angular.forEach(temp, function(index,value){
+      arr.push(value);
+    })
+    return arr;
+  }
+  $scope.ok = function () {
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
